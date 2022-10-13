92449d36acac6c16e197a16e9892090cffa9886c
TichonovRoman <romikt@inbox.ru>
2022-10-13 20:20:51 +0300

    свайпы

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Alert, TouchableWithoutFeedback, FlatList, View, Keyboard, ListRenderItem, StyleSheet, Text, TextInput, TouchableOpacity, Pressable } from 'react-native';
+import 'react-native-gesture-handler';
+import { Alert, TouchableWithoutFeedback, FlatList, View, Keyboard,
+    ListRenderItem, StyleSheet, Text, TextInput, TouchableOpacity, Pressable, Animated } from 'react-native';
 import { MemoIcon1 } from './icon/icon1';
 import { MemoIcon2 } from './icon/icon2';
 import { MemoIcon3 } from './icon/icon3';
-
+import Swipeable from 'react-native-gesture-handler/Swipeable'
+import {GestureHandlerRootView} from 'react-native-gesture-handler'
 
 
 
@@ -24,28 +27,53 @@ const [tasks, setTasks] = useState<TasksType[]>([
   {id: 5, title: 'React Native', isDone: false},
 ])
 
+const Right = () =>{
+  return <View><Text>Right</Text></View>
+}
+
+// const Right = () =>{
+//   return <View style={{flexDirection: 'row', marginVertical: 5}}>
+//     <Text style={{backgroundColor: 'red'}}>Delete</Text>
+//     <Text style={{backgroundColor: 'green'}}>Select</Text>
+//     <Text style={{backgroundColor: 'grey'}}>Pin</Text>
+//     </View>
+// }
+
+
 const [inputValue, setInputValue] = useState<string>('')
 
 const changeStatus = (id: number) => {
   const changedTasksStatusArray = tasks.map((task)=> task.id  === id ? {...task, isDone: !task.isDone} : task)
   setTasks(changedTasksStatusArray)
-
 }
 
 const render: ListRenderItem<TasksType> = ({item})=>{
-  return <TouchableWithoutFeedback onPress={()=>changeStatus(item.id)}>
-<View
-    style={[styles.task, {
-    borderWidth: 2,
-    opacity: item.isDone ? 0.4 : 1,
-    borderColor: item.isDone ? 'green' : 'red'
-   
-    }]}>
-      <Text style={{fontSize: 24, fontWeight: '500', paddingVertical: 10}}>
-        {item.title}
-      </Text>
-       </View>
-  </TouchableWithoutFeedback> 
+  return <Swipeable renderLeftActions={(progressAnimatedValue, dragAnimatedValue)=>{
+      const trans = dragAnimatedValue.interpolate({
+          inputRange: [-80,0],
+          outputRange: [1,0],
+          extrapolate: 'clamp'
+      });
+      return <View style={{flexDirection: 'row', marginVertical: 5}}>
+          <Animated.Text style={[{backgroundColor: 'red', width: 10}, {transform: [{ scale: trans }]}]}>Delete</Animated.Text>
+          <Animated.Text style={[{backgroundColor: 'green', width: 10}, {transform: [{ scale: trans }]}]}>Select</Animated.Text>
+          <Animated.Text style={[{backgroundColor: 'grey', width: 10}, {transform: [{ scale: trans }]}]}>Pin</Animated.Text>
+      </View>
+  }} renderRightActions={Right}>
+    <TouchableWithoutFeedback onPress={()=>changeStatus(item.id)}>
+  <View
+      style={[styles.task, {
+      borderWidth: 2,
+      opacity: item.isDone ? 0.4 : 1,
+      borderColor: item.isDone ? 'green' : 'red'
+     
+      }]}>
+        <Text style={{fontSize: 24, fontWeight: '500', paddingVertical: 10}}>
+          {item.title}
+        </Text>
+         </View>
+    </TouchableWithoutFeedback> 
+    </Swipeable> 
  }
  const addTask = ()=>{
   if (inputValue.length>0) {
@@ -58,9 +86,11 @@ const render: ListRenderItem<TasksType> = ({item})=>{
   } else Alert.alert('введите название задачи')
  
  }
-
+ // flex: 1 означает что эта обертка занимает все пространство экрана
   return (
-    <View style={styles.container}>
+    
+    <GestureHandlerRootView style={{flex: 1}}>
+<View style={styles.container}>
       <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
         <TextInput style={styles.input} value={inputValue} onChangeText={setInputValue}/>
         <TouchableOpacity onPress={addTask}>
@@ -89,6 +119,8 @@ const render: ListRenderItem<TasksType> = ({item})=>{
       
       <StatusBar style="auto" />
     </View>
+    </GestureHandlerRootView>
+    
   );
 }
 
@@ -107,10 +139,10 @@ const styles = StyleSheet.create({
   },
   task: {
     borderWidth: 1, 
+    backgroundColor: '#fff',
     borderBottomLeftRadius: 20, 
     borderTopRightRadius: 20, 
     alignItems: 'center', 
     marginVertical: 5
-
   }
 });
